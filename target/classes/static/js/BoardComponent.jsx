468d90c6ff130758e16fbbb1a2fbdb0f2f58e0a9
Nicolas Piñeros Campo <onpc1224@gmail.com>
2022-06-27 21:52:46 -0500

    setup canvas en react

--- target/classes/static/js/BoardComponent.jsx
+++ target/classes/static/js/BoardComponent.jsx
@@ -1,63 +1,39 @@
 class BoardComponent extends React.Component {
   constructor(props) {
-    createCanvas(640, 480);
     super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      status: "",
-    };
+    this.myRef = React.createRef();    
   }
+
+  Sketch = (p) => {
+    p.setup = () => {
+      p.createCanvas(screen.width-60, screen.height-200);
+    };
+
+    p.draw = () => {
+      if (p.mouseIsPressed === true) {
+        p.fill(0, 0, 0);
+        p.ellipse(p.mouseX, p.mouseY, 20, 20);
+      }
+      if (p.mouseIsPressed === false) {
+        p.fill(255, 255, 255);
+      }
+    };
+  };
 
   componentDidMount() {
-    this.timerID = setInterval(() => this.checkStatus(), 1000);
-    this.timerID = setInterval(() => this.draw(), 1000);
+    this.myP5 = new p5(this.Sketch, this.myRef.current);        
   }
 
-  checkStatus() {
-    fetch("/status")
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            status: result.status,
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
-        }
-      );
-  }
-
-  draw() {
-    if (mouseIsPressed === true) {
-      fill(0, 0, 0);
-      ellipse(mouseX, mouseY, 20, 20);
-    }
-    if (mouseIsPressed === false) {
-      fill(255, 255, 255);
-    }
-  }
 
   render() {
-    const { error, isLoaded, status } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
+    
       return (
-        <div>
-          <h1>The server status is:</h1>
-          <p>{status}</p>
-        </div>
+        
+        <div ref={this.myRef}></div>
+        
       );
-    }
-  }
+      }
+  
 }
 
 ReactDOM.render(<BoardComponent />, document.getElementById("board"));
